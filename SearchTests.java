82facb6e6c27820ea2ef236a4d814585fc4b7829
looogfyr <alexdubinin88@gmail.com>
2025-03-11 20:57:18 +0900

    hw 1 - done!

---
+++ SearchTests.java
@@ -0,0 +1,9 @@
+public class SearchTests {
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+}
+}
